d8c249f1200b3f9e00cb89bc153d490ba5e888ac
JisBH <106785363+JisBH@users.noreply.github.com>
2025-10-02 11:35:37 +0200

    Add files via upload

---
+++ Practica1_AMC/src/main/java/pruebagit/practica1_amc/Practica1_AMC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pruebagit.practica1_amc;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Practica1_AMC {
+
+    public static void main(String[] args) {
+        
+    }
+}
