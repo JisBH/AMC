68063d05eb71060c65ac3bbd9fcc16810b5e0444
Yassin <156797300+ysssssn@users.noreply.github.com>
2025-12-04 11:49:45 +0100

    borro Practica2_AMC directory
    al final no es esa la q se hace

--- Practica2_AMC/Practica 2 AMC/src/Modelo/Proceso.java
+++
@@ -1,9 +0,0 @@
-package Modelo;
-
-public interface Proceso {
-    public abstract boolean esFinal(String estado); 
-    public abstract boolean reconocer(String cadena);
-
-    @Override
-    public abstract String toString();
-}
